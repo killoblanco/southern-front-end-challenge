463a79d2aaebaf0c17f1cb909c8d0c16b2cc63ec
Kamilo Vasquez <killo.blanco@gmail.com>
2023-06-28 23:19:43 -0500

    ✨ New Photo Grid component

---
+++ src/components/photo-grid.tsx
@@ -0,0 +1,27 @@
+import { type GetPhotosResponse } from '@/mars/types'
+import { ImageList, ImageListItem, ImageListItemBar, Typography } from '@mui/material'
+
+interface Props {
+  photos: GetPhotosResponse
+}
+
+export const PhotoGrid: React.FC<Props> = ({ photos }) => {
+  if (photos.length === 0) {
+    return (
+      <Typography variant="h4" align="center">
+        No photos found.
+      </Typography>
+    )
+  }
+
+  return (
+    <ImageList variant="masonry" cols={3} gap={16}>
+      {photos.map((photo, idx) => (
+        <ImageListItem key={photo.id}>
+          <img src={photo.src} alt={`photo-${photo.id}`} />
+          <ImageListItemBar title={photo.camera} />
+        </ImageListItem>
+      ))}
+    </ImageList>
+  )
+}
