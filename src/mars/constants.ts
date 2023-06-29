4ccdea485a81ed44df18dd7fcd271ad1d944f869
Kamilo Vasquez <killo.blanco@gmail.com>
2023-06-28 23:22:30 -0500

    ✨ New App constants

---
+++ src/mars/constants.ts
@@ -0,0 +1,29 @@
+import { type Cameras, type PhotosFilters } from '@/mars/types'
+
+export const BASE_API_URL = 'https://api.nasa.gov/mars-photos/api/v1'
+export const ROVERS = ['curiosity', 'opportunity', 'spirit'] as const
+
+export const CAMERAS: Cameras[] = [
+  { value: 'all', label: 'All Cameras', rover: ['curiosity', 'opportunity', 'spirit'] },
+  { value: 'fhaz', label: 'Front Hazard Avoidance Camera', rover: ['curiosity', 'opportunity', 'spirit'] },
+  { value: 'rhaz', label: 'Rear Hazard Avoidance Camera', rover: ['curiosity', 'opportunity', 'spirit'] },
+  { value: 'mast', label: 'Mast Camera', rover: ['curiosity'] },
+  { value: 'chemcam', label: 'Chemistry and Camera Complex', rover: ['curiosity'] },
+  { value: 'mahli', label: 'Mars Hand Lens Imager', rover: ['curiosity'] },
+  { value: 'mardi', label: 'Mars Descent Imager', rover: ['curiosity'] },
+  { value: 'navcam', label: 'Navigation Camera', rover: ['curiosity', 'opportunity', 'spirit'] },
+  { value: 'pancam', label: 'Panoramic Camera', rover: ['opportunity', 'spirit'] },
+  {
+    value: 'minites',
+    label: 'Miniature Thermal Emission Spectrometer (Mini-TES)',
+    rover: ['opportunity', 'spirit']
+  }
+]
+
+export const DEFAULT_FILTERS: PhotosFilters = {
+  rover: 'curiosity',
+  camera: 'all',
+  earthDate: undefined,
+  sol: 1,
+  page: 1
+}
