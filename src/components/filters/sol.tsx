e1d4eebe31e2b37eb62d2ca5ad536eb0e50f3926
Kamilo Vasquez <killo.blanco@gmail.com>
2023-06-28 23:19:23 -0500

    ✨ New Sol Date filter

---
+++ src/components/filters/sol.tsx
@@ -0,0 +1,54 @@
+import { DEFAULT_FILTERS } from '@/mars/constants'
+import { Slider, Typography } from '@mui/material'
+import { useRouter } from 'next/router'
+import { useEffect, useState } from 'react'
+
+interface Props {
+  max: number
+}
+
+export const SolFilter: React.FC<Props> = ({ max }) => {
+  const { query, push } = useRouter()
+  const [sol, setSol] = useState(parseInt((query.sol ?? DEFAULT_FILTERS.sol) as string, 10))
+
+  const marks = [
+    { value: 1, label: '1' },
+    ...(Array.from(
+      { length: 5 },
+      (_, idx) => {
+        if (idx === 4) return max
+        return Math.floor((max / 5) * (idx + 1))
+      }).map((value) => ({ value, label: value.toString() })))
+  ]
+
+  useEffect(() => {
+    delete query.earthDate
+    void push({
+      pathname: '/',
+      query: {
+        ...query,
+        sol,
+        page: 1
+      }
+    })
+  }, [sol])
+
+  const handleOnChange = (event: React.SyntheticEvent | Event, sol: number | number[]): void => {
+    setSol(sol as number)
+  }
+
+  return (
+    <>
+      <Typography>Sol Date</Typography>
+      <Slider
+        step={1}
+        value={sol}
+        onChangeCommitted={handleOnChange}
+        min={1}
+        max={max}
+        marks={marks}
+        valueLabelDisplay="auto"
+      />
+    </>
+  )
+}
